7b6155333fdf96dde7010b5ad2eb26ef54c52a0d
Ayush Singh <81627141+Ayush181005@users.noreply.github.com>
2022-06-06 12:09:02 +0530

    Update Functions.py

--- Functions.py
+++ Functions.py
@@ -4,13 +4,6 @@ def say_hi():
 say_hi()
 
 #Another way:
-def ilovedhairya(name, age):
-    print("Ayush loves "+name+". She is vey beautiful and sexy, she loves Ayush truly. Her age is "+age+". She wants to do sex with Ayush in college before marriage and same with Ayush. She will marry Ayush and have babies together.")
-    print("Ayush will insert his Penis inside Dhairya's sexy vagina and Dhairya's sexy big boobs will touch Ayush's face in the future.")
-    print("Dhairya will sit nude in Ayush's laps and hug him(both nude) and then kiss him(lip to lip).And you can imagine...")
-    print(" ___ 0 ___")
-    print("    O O")
-    print("    |.|")
-    print("    / \\")
-    print("   /   \\")
-ilovedhairya("Dhairya", "15")
+def paramFunf(name, age):
+    print("Hi "+name+".. age is "+age+".
+ParamFunc("Ayush", "15")
